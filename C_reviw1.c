eadcac4c335d0adfbad543ae7b018efdeb3cea4b
datzzky <79579414+datzzky@users.noreply.github.com>
2021-03-22 00:00:00 +0800

    Add files via upload
    a program ask the user two integers then add, multiple, divide and subtract them

---
+++ C_reviw1.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+
+int main(){
+	int num_1, num_2, result;
+	char choice;
+	printf("Enter two numbers to calculate\n"
+			"\nEnter + to add"
+			"\nEnter - to subtract"
+			"\nEnter * to multiply"
+			"\nEnter / to devide"
+			"\n\nnum 1, operator, num2: "
+	       );
+	scanf("%d %c %d", &num_1, &choice, &num_2);
+		if(choice == '+')
+		{
+			result = num_1 + num_2;
+		}
+		else if(choice == '-')
+		{
+			result = num_1 - num_2;
+		}
+		else if(choice == '*')
+		{
+			result = num_1 * num_2;
+		}
+		else if(choice == '/')
+		{
+			result = num_1 / num_2;
+		}
+		printf("\nresult = %d", result);
+		getch();
+		return 0;
+	}
