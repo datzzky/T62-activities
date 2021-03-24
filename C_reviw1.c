5cc1a224534a3739afc487974fff1fd319378a2e
datzzky <79579414+datzzky@users.noreply.github.com>
2021-03-25 01:20:27 +0800

    Add files via upload

--- C_reviw1.c
+++ C_reviw1.c
@@ -1,33 +1,15 @@
 ﻿#include <stdio.h>
 
 int main(){
-	int num_1, num_2, result;
-	char choice;
+	float num_1, num_2, sum, product, Deference , qoutient;
 	printf("Enter two numbers to calculate\n"
-			"\nEnter + to add"
-			"\nEnter - to subtract"
-			"\nEnter * to multiply"
-			"\nEnter / to devide"
-			"\n\nnum 1, operator, num2: "
 	       );
-	scanf("%d %c %d", &num_1, &choice, &num_2);
-		if(choice == '+')
-		{
-			result = num_1 + num_2;
-		}
-		else if(choice == '-')
-		{
-			result = num_1 - num_2;
-		}
-		else if(choice == '*')
-		{
-			result = num_1 * num_2;
-		}
-		else if(choice == '/')
-		{
-			result = num_1 / num_2;
-		}
-		printf("\nresult = %d", result);
+	scanf("%f %f", &num_1, &num_2);
+		sum = num_1 + num_2;
+		Deference  = num_1 - num_2;
+		product = num_1 * num_2;
+		qoutient = num_1 / num_2;
+		printf("\nsum = %.2f\nproduct = %.2f\ndeference = %.2f\nqoutint = %.2f", sum, product, Deference , qoutient);
 		getch();
 		return 0;
 	}
