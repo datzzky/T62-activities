d1808bd51d358f4b22399a90d2931631cc7a3823
datzzky <79579414+datzzky@users.noreply.github.com>
2021-03-25 01:23:06 +0800

    Add files via upload
    a C Program that checks positive, negative, zero or invalid inputted integer using switch case

---
+++ C_review3.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int num;
+	printf("Enter a number: ");
+	scanf("%d", &num);
+	switch(num > 0)
+       {
+		case 0:
+			switch(num <0)
+			{
+				case 1:
+						printf("Negative Integer");
+						break;
+				case 0:
+						printf("Zero number");
+						break;
+			}
+			break;
+        case 1:
+               printf("positive Integer");
+			   break;
+		default:
+				printf("invalid Value");
+				break;
+  
+        }
+		getch();
+	return 0;
+}
