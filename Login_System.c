dff694aee102ab92ccf5a4c5c8fec60040352d9f
datzzky <79579414+datzzky@users.noreply.github.com>
2021-04-13 17:53:08 +0800

    Add files via upload

---
+++ Login_System.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+int pin, id_num, Num_of_users, i;
+// array of users
+int Users[][2] ={
+	{200110, 2468},
+	{200111, 1357},
+	{200112, 1234}
+};
+void validate_id(int id_num);
+void pin_validate(int i, int pin);
+int main()
+{
+//calculating the actual size of rows
+	Num_of_users = sizeof(Users) / sizeof(Users[2]);
+
+	printf("Enter your id number: ");
+	scanf("%d", &id_num);
+	validate_id(id_num);
+    return 0;
+}
+// use to valiate id numbers
+void validate_id(int id_num)
+{
+	for(i = 0;i < Num_of_users;i++)
+	{
+		if(id_num == Users[i][0])
+		{
+			printf("Enter your pin: ");
+			scanf("%d", &pin);
+			pin_validate(i,pin);
+			break;
+		}
+		if(!(id_num == Users[i][0]))
+		{	if(i == Num_of_users - 1)
+			{
+				printf("id is not listed");
+			}
+		}
+	}
+}
+void pin_validate(int i, int pin)
+{
+	system("cls");
+	if(pin == Users[i][1])
+	{
+		printf("You are now login");
+	}
+	else
+	{
+		while(!(pin == Users[i][1]))
+		{
+			printf("Incorect password re enter: ");
+			scanf("%d", &pin);
+			pin_validate(i, pin)
+		}
+	}
+}
+/*
+explaination
+for loop in the function "validate_id" is use to look for the list of id number in the rows of index 0.
+the pin_validate function is use to validate pin using the index valu of [i][1] where i is the users id index posistion.
