17ff0db19a3086808766d7fb61da28ff961800e0
datzzky <79579414+datzzky@users.noreply.github.com>
2021-05-24 21:59:28 +0800

    Add files via upload

---
+++ EA1_ Extra_Activity_File _Handling.c
@@ -0,0 +1,61 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+/*variable section*/
+char Name[25];
+int age,i, j, sub_size, id_Num;
+char sub[][10] = {/*subject array*/
+	"MATH",	"SCEINCE",	"ENGLISH"};
+float sum, avrg;
+/*file section*/
+FILE *studnt_ptr;
+char stdnts_files[][15] ={ "file1.txt", "file2.txt"};
+int main()
+{
+/*this is to get the total number of subject array.*/
+	int sub_size = sizeof(sub)/sizeof(sub[0]);
+/*this to get the total number of file array.*/
+	int files_count = sizeof(stdnts_files)/sizeof(stdnts_files[0]);
+/*this loop is keep on repeating until last txt file is finish.*/
+	int grades[sub_size];/*local varialble for grades*/
+	while(j < files_count)
+	{
+		printf("\t\tInput Data for File %d\n\n", j+1);
+		printf("Input Name: ");
+		fgets(Name, sizeof(Name), stdin);
+		printf("Input ID no.: ");
+		scanf("%d", &id_Num);
+		printf("Input Age: ");
+		scanf("%d", &age);
+		studnt_ptr = fopen(stdnts_files[j], "w");
+		fprintf(/*/to print into the file*/
+		studnt_ptr,
+		"Name: %sID No.:%d\nage: %d\n",
+		Name, id_Num, age
+		);
+/*for loop is use to get and print the grades.*/
+		printf("Enter grades\n");
+	for(i = 0; i < sub_size; i++)
+	{
+		printf("%s : ", sub[i]);
+		scanf("%d", &grades[i]);
+		fprintf(studnt_ptr,
+		"%s: %d\n",
+		sub[i], grades[i]);
+		sum = sum + grades[i];
+	}	
+	avrg = sum/sub_size;
+	fprintf(studnt_ptr, "sum: %.2f\naverage: %.2f", sum, avrg);
+	fclose(studnt_ptr);
+	sum =0;/*this is to set back the sum into 0*/
+	avrg = 0; /*same as sum*/ 
+	j++;
+	system("cls");
+	}
+	printf("done!");
+	return 0;
+}
+/*
+
+
+*/
+
---
+++ M4FT_File_Handling_Activity.c
@@ -0,0 +1,62 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#define MAX 5
+/*files*/
+FILE * data;
+/*variable section*/
+char txt_in[10];
+char txt_out[10];
+char opt;
+int i;
+/*function section*/
+void write();
+void display();
+/*body*/
+int main()
+{
+
+		write();
+		system("cls");
+		printf("\tText from the file[MyFile.txt]\n\n");
+		display();
+	return 0;
+}
+
+void write()
+{
+	data = fopen("MyFile.txt", "w");
+	if(data == NULL)
+	{
+		printf("cannot open the file!");
+	}else{
+	for(i =0;i < MAX; i++)
+	{
+		printf("Input Data: " );
+		scanf("%s\n", txt_in);
+		fprintf(data, "%s\n", txt_in);
+	}
+		fclose(data);
+}
+	
+	printf("write succes! press any key to confirm");
+	getchar();
+/*I used getchar to make a short delay este when the function is exited it did'nt write anything.*/
+}
+
+void display()
+{
+	data = fopen("MyFile.txt", "r");
+	if(data == NULL)
+	{
+		printf("\t\t<access denied!>\n\n");
+		exit(1);
+	}else
+	{
+		for(i =0;i < MAX; i++)
+		{
+			fscanf(data,"%s", txt_out);
+			printf("%s\n", txt_out);
+		}
+	}
+	fclose(data);
+}
